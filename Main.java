a8713a5ea2734ee2e300082b46d5fae1317439d0
MDorosz <126687548+MDorosz@users.noreply.github.com>
2023-05-24 12:26:38 +0000

    ostateczna wersja

--- Main.java
+++ Main.java
@@ -1,5 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello!");
+
+    Random random = new Random();
+    int wylosowanalb = random.nextInt(101);
+    int zgadnietalb;
+    Scanner scanner = new Scanner(System.in);
+    System.out.println(wylosowanalb);
+
+   
+    do {
+       System.out.println("Zgadnij wylosowana liczbe z zakresu od 0 do 100:");
+      zgadnietalb = scanner.nextInt();
+      if (zgadnietalb < wylosowanalb) System.out.println("Wylosowana liczba jest wieksza spróboj jeszcze raz");
+      if (zgadnietalb > wylosowanalb) System.out.println("Wylosowana liczba jest mniejsza spróboj jeszcze raz");
+      if (zgadnietalb == wylosowanalb) System.out.println("!!!Gratulacje trafiles!!!");
+    } while (wylosowanalb != zgadnietalb);
+    
   }
 }
